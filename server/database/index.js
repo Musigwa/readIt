2f1bdffb5ffc0721d64a0d95c32805d33eb86d96
musigwa <pacifique.musigwa@gmail.com>
2019-01-29 10:05:04 +0200

    [Chore #163533980] Prepare the project's environment configuration and structure

--- server/database/index.js
+++
@@ -1,9 +0,0 @@
-import config from 'config';
-import Sequelize from 'sequelize';
-
-const connectionString = config.get('DATABASE_URL');
-export default class Database {
-  static connection() {
-    return new Sequelize(connectionString);
-  }
-}
