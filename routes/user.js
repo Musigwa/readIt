bc41d7bbe8b1b077cd45aa0a153fa751bfa13637
luc-abayo <luc.bayo@gmail.com>
2019-01-30 14:28:03 +0200

    [ft-user-login-#163518493]Add authentication with jwt token

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,17 @@
 import { Router } from 'express';
+import passport from 'passport';
 import User from '../controllers/users';
 
 const userRouters = Router();
 
 userRouters
   .post('/users', User.create)
-  .get('/users', User.getAllUser)
-  .get('/users/:id', User.getOneUser)
-  .put('/users/:id', User.update);
+  .get('/users', passport.authenticate('jwt', { session: false }), User.getAllUser)
+  .get(
+    '/users/:id',
+    passport.authenticate('jwt', { session: false }),
+    User.getOneUser,
+  )
+  .put('/users/:id', passport.authenticate('jwt', { session: false }), User.update);
 
 export default userRouters;
